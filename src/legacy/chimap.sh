b37f846cf455c05006c8ec5268d4772523e096cf
Tim <timothy.daniel.roberts@gmail.com>
2013-07-01 20:53:01 +0100

    added new Makefile for profiling and some basic profiling data in /profile using gprof

--- src/legacy/chimap.sh
+++ src/legacy/chimap.sh
@@ -4,6 +4,9 @@
 #SBATCH --job-name="dQSM"
 #SBATCH --output="out%j.txt"
 
+if [ -d "out" ]; then
+    rm -rf out
+fi
 outdir=out
 mkdir $outdir
 
@@ -12,10 +15,10 @@ data="-DeltaB $datadir/deltab.bin"
 mask="-mask $datadir/mask.bin"
 modelmap="-modelmap $datadir/models.bin"
 out="-out $outdir"
-maxiters="-maxiters 2"
+maxiters="-maxiters 20"
 model="-model m"
 alpha="-alpha 0.5"
-np="-np 2"
+np="-np 4"
 #previter="-x out41736/x_iter000925.bin"
 
 #echo mpirun $np ./chimap $model $data $mask $modelmap $out $maxiters $alpha
