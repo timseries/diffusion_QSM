6127ba2f5cf29ac9fa82c7081de7698796570512
Timothy Roberts (Collaboratory) <timroberts@avoca.(none)>
2013-07-24 09:55:25 +1000

    adding some slick mods to the dqsm.sh file so this is all you have to run...halfway through

--- dqsm.sh
+++ dqsm.sh
@@ -1,32 +1,28 @@
 #!/bin/sh
 
-if [ $1 = "hybrid" ]; then
-    executable="src/hybrid/dqsm"
-fi
-outdir=out/$1
-if [ -d $outdir ]; then
-    rm -rf $outdir
-fi
-
-if [ -d out ]; then
-    echo "out exists"
-else
-    mkdir out
-fi
-
-mkdir $outdir
 
 executable=./src/$1/dqsm
 datadir=./data/legacy
 data="-DeltaB $datadir/deltab.bin"
 mask="-mask $datadir/mask.bin"
 modelmap="-modelmap $datadir/models.bin"
-out="-out $outdir"
-maxiters="-maxiters 20"
+maxiters="-maxiters $3"
 model="-model m"
 alpha="-alpha 0.5"
-np="-np 2"
+np="-np $2"
+out="-out $outdir"
+
 #previter="-x out41736/x_iter000925.bin"
+
+today=$(date "+%d%m%Y")
+outdir=out/$1/$today/
+if [ -d $outdir ]; then
+    rm -rf $outdir
+else
+    mkdir -p $outdir
+fi
+mkdir $outdir
+
 
 #echo mpirun $np ./chimap $model $data $mask $modelmap $out $maxiters $alpha
 mpirun $np $executable $model $data $mask $modelmap $out $maxiters $alpha
